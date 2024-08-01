26fb710123476275fa79ed617c10a2b138087929
Gustavo Koglin <engcomputacao.gustavokoglin@gmail.com>
2024-07-31 20:53:38 -0400

    Update environment.ts
    Removendo chave de API do Google

--- JobFind_FrontEnd/src/environments/environment.ts
+++ JobFind_FrontEnd/src/environments/environment.ts
@@ -1,13 +1,1 @@
-export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000',
-  firebaseConfig: {
-    apiKey: "AIzaSyAjU_EWiqKNFwzSLrospKPX8fNB_jv1KeI",
-    authDomain: "jobfind-2024.firebaseapp.com",
-    projectId: "jobfind-2024",
-    storageBucket: "jobfind-2024.appspot.com",
-    messagingSenderId: "262468371716",
-    appId: "1:262468371716:web:27a6537ce5b402f8ddfc51",
-    measurementId: "G-B5THK127WR"
-  }
-};
+
