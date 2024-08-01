6a71d13af2f942fe48a8e3f3cfdb73286b70a4da
Gustavo Koglin <engcomputacao.gustavokoglin@gmail.com>
2024-07-31 20:54:08 -0400

    Update environment.prod.ts

--- JobFind_FrontEnd/src/environments/environment.prod.ts
+++ JobFind_FrontEnd/src/environments/environment.prod.ts
@@ -1,13 +1,1 @@
-// export const environment = {
-//   production: true,
-//   apiUrl: 'https://api.yourproductionurl.com', // Atualize para a URL real da API de produção
-//   firebaseConfig: {
-//     apiKey: "AIzaSyAjU_EWiqKNFwzSLrospKPX8fNB_jv1KeI",
-//     authDomain: "jobfind-2024.firebaseapp.com",
-//     projectId: "jobfind-2024",
-//     storageBucket: "jobfind-2024.appspot.com",
-//     messagingSenderId: "262468371716",
-//     appId: "1:262468371716:web:27a6537ce5b402f8ddfc51",
-//     measurementId: "G-B5THK127WR"
-//   }
-// };
+
