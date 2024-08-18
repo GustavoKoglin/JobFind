c327c6afdf3d8e7022c50717de2560a023d8ec91
Gustavo Koglin <engcomputacao.gustavokoglin@gmail.com>
2024-08-18 00:14:10 -0400

    Update 18.08.2024

--- JobFind_BackEnd/server.js
+++ JobFind_BackEnd/server.js
@@ -11,6 +11,30 @@ server.post('/auth/empresa/registrar', function(req, res) {
     res.send({ message: 'Empresa registrada com sucesso!' });
 });
 
+server.get('/perfil/candidato', function(req, res) {
+    res.send({ message: 'Perfil do candidato' });
+});
+
+server.get('/perfil/empresa', function(req, res) {
+    res.send({ message: 'Perfil da empresa' });
+});
+
+server.patch('/candidato/editar', function(req, res) {
+    res.send({ message: 'Candidato atualizado com sucesso!' });
+});
+
+server.patch('/empresa/editar', function(req, res) {
+    res.send({ message: 'Empresa atualizada com sucesso!' });
+});
+
+server.delete('/candidato/excluir', function(req, res) {
+    res.send({ message: 'Candidato excluído com sucesso!' });
+});
+
+server.delete('/empresa/excluir', function(req, res) {
+    res.send({ message: 'Empresa excluída com sucesso!' });
+});
+
 server.listen({
     port: 3000,
 })
