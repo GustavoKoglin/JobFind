ad558eceef130a206968bc270c39957f6e84f844
Gustavo Koglin <engcomputacao.gustavokoglin@gmail.com>
2024-08-18 00:18:28 -0400

    Adding arrow function on server - Update 18.08.2024

--- JobFind_BackEnd/server.js
+++ JobFind_BackEnd/server.js
@@ -3,35 +3,35 @@ import { fastify } from 'fastify';
 const server = fastify();
 
 
-server.post('/auth/candidato/registrar', function(req, res) {
+server.post('/auth/candidato/registrar', (req, res) => {
     res.send({ message: 'Candidato registrado com sucesso!' });
 });
 
-server.post('/auth/empresa/registrar', function(req, res) {
+server.post('/auth/empresa/registrar', (req, res) => {
     res.send({ message: 'Empresa registrada com sucesso!' });
 });
 
-server.get('/perfil/candidato', function(req, res) {
+server.get('/perfil/candidato', (req, res) => {
     res.send({ message: 'Perfil do candidato' });
 });
 
-server.get('/perfil/empresa', function(req, res) {
+server.get('/perfil/empresa', (req, res) => {
     res.send({ message: 'Perfil da empresa' });
 });
 
-server.patch('/candidato/editar', function(req, res) {
+server.patch('/candidato/editar', (req, res) => {
     res.send({ message: 'Candidato atualizado com sucesso!' });
 });
 
-server.patch('/empresa/editar', function(req, res) {
+server.patch('/empresa/editar', (req, res) => {
     res.send({ message: 'Empresa atualizada com sucesso!' });
 });
 
-server.delete('/candidato/excluir', function(req, res) {
+server.delete('/candidato/excluir', (req, res) => {
     res.send({ message: 'Candidato excluído com sucesso!' });
 });
 
-server.delete('/empresa/excluir', function(req, res) {
+server.delete('/empresa/excluir', (req, res) => {
     res.send({ message: 'Empresa excluída com sucesso!' });
 });
 
